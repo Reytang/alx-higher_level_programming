04b64baa55c5e53ba5aaeb1a21f1c1c089045097
reytang <nubaboub@gmail.com>
2023-05-29 05:05:41 -0700

    exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    fx = 0
+    while idx < x:
+        try:
+            print("{}".format(my_list[fx]), end="")
+        except IndexError:
+            break
+        fx += 1
+    print("")
+    return fx
