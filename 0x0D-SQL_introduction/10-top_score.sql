5a296be7989fc74050e8b723e27df8ba7700d6f5
reytang <nubaboub@gmail.com>
2023-07-18 08:04:08 -0700

    sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- script that lists all records of the table second_table of the database
+-- Results should display both the score and the name
+
+SELECT score, name FROM second_table ORDER BY score DESC;
