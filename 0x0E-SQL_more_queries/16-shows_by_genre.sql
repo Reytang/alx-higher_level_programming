60f537c8383f23baa2af906a53c1168019077f42
reytang <nubaboub@gmail.com>
2023-07-19 08:54:52 -0700

    sql more 14-16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,16 @@
+-- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
+-- run this first to import a SQL dump -->
+--      echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
+--      curl [link] -s | mysql -uroot -p hbtn_0d_tvshows
+--
+-- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+-- if a show doesn’t have a genre, display NULL in the genre column
+-- each record should display: tv_shows.title - tv_genres.name
+
+    SELECT tv_shows.title, tv_genres.name
+      FROM tv_shows
+ LEFT JOIN tv_show_genres
+        ON tv_shows.id = tv_show_genres.show_id
+ LEFT JOIN tv_genres
+	ON tv_show_genres.genre_id = tv_genres.id
+  ORDER BY tv_shows.title, tv_genres.name ASC;
