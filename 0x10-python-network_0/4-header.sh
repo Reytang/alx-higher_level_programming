d3547beb3347ff60d12af1f058da407391ec89a5
reytang <nubaboub@gmail.com>
2023-08-31 12:53:16 -0700

    updated

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -2,4 +2,5 @@
 # acript that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
 
 # A header variable X-School-User-Id must be sent with the value 98
-curl -s -H "X-School-User-Id":98 "$1"
+
+curl -s "$1" -H "X-School-User-Id: 98"
