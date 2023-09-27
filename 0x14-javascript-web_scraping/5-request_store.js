34acf31958c400335c89a53676a8eb732e756757
Reytang <nubaboub@gmail.com>
2023-09-26 17:32:55 -0700

    corrected

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
 
-let url = process.argv[2];
-let filename = process.argv[3];
+const url = process.argv[2];
+const filename = process.argv[3];
 const fs = require('fs');
 const request = require('request');
-
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filename, body, 'utf8');
-  }
-});
+request(url).pipe(fs.createWriteStream(filename));
